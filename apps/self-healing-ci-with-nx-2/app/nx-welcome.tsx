a7267092bd5a5907574eaf94c8a8776410bb8d39
Jonathan Cammisuli <4332460+Cammisuli@users.noreply.github.com>
2025-11-06 13:41:56 -0500

    remove default font

--- apps/self-healing-ci-with-nx-2/app/nx-welcome.tsx
+++ apps/self-healing-ci-with-nx-2/app/nx-welcome.tsx
@@ -13,7 +13,6 @@ export function NxWelcome({ title }: { title: string }) {
           __html: `
     html {
       -webkit-text-size-adjust: 100%;
-      font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
       'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif,
       'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
       'Noto Color Emoji';
